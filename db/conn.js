bab28758cb684f1ccddafce48e4b44b7a697c481
kabwoy <deejaykabwoy@gmail.com>
2023-01-28 09:43:20 +0300

    another change

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,3 @@
 const {Sequelize} = require("sequelize")
-const sequelize = new Sequelize({username:process.env.DB_USERNAME , database:process.env.DATABASE , password:process.env.DB_PASSWORD , dialect:'mysql' , host:process.env.DB_HOST})
+const sequelize = new Sequelize({username:process.env.DB_USERNAME , database:process.env.DATABASE , password:process.env.DB_PASSWORD , dialect:'mysql' , host:process.env.DB_HOST , port:process.env.DB_PORT})
 module.exports = sequelize
